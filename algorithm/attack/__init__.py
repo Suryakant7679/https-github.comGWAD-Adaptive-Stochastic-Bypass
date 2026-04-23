3556eb57f49ec2f845e119656bc114cb07cf292c
Surya kant mani <122162235+Suryakant7679@users.noreply.github.com>
2026-04-23 17:35:00 +0530

    Update __init__.py

--- algorithm/attack/__init__.py
+++ algorithm/attack/__init__.py
@@ -1,6 +1,6 @@
 from .blackbox.techniques.nes.nes import NES
 from .blackbox.techniques.hsja.hsja import HSJA
 from .blackbox.techniques.sign_flip.sign_flip import SignFlip
-
+from .blackbox.techniques.camouflage.camouflage_attack import CamouflageAttack
 # attack
 __version__ = 0.1
